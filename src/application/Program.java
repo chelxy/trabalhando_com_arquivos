a1a8306c2e7a21535133057b84f4d7b18b391b1a
Michel Morais <valentimmichel@gmail.com>
2023-02-24 21:37:12 -0300

    Exercicio proposto

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,22 +1,64 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
-public class Program {
-//Informações do caminho do arquivo
-	public static void main(String[] args) {
+import entities.Product;
 
+public class Program {
+//Exercicio proposto
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
+		List<Product> list = new ArrayList<>();
+
 		System.out.print("Enter a file path: ");
-		String strPath = sc.nextLine();
+		String strFile = sc.nextLine();
 
-		File path = new File(strPath);
+		File file = new File(strFile);
+		String strFolder = file.getParent();
 
-		System.out.println("getName: " + path.getName());//somente o nome do arquivo
-		System.out.println("getParent: " + path.getParent());//somente o caminho
-		System.out.println("getPath: " + path.getPath());//todo o caminho
+		boolean success = new File(strFolder + "\\out").mkdir();//Criação de uma subpasta "out"
+		String targetFile = strFolder + "\\out\\summary.csv";
+
+		try (BufferedReader br = new BufferedReader(new FileReader(strFile))) {
+
+			String itemCsv = br.readLine();
+			while (itemCsv != null) {
+
+				String[] fields = itemCsv.split(",");
+				String name = fields[0];
+				double price = Double.parseDouble(fields[1]);
+				int quantity = Integer.parseInt(fields[2]);
+
+				list.add(new Product(name, price, quantity));
+
+				itemCsv = br.readLine();
+			}
+			try (BufferedWriter bw = new BufferedWriter(new FileWriter(targetFile))) {
+
+				for (Product item : list) {
+					bw.write(item.getName() + ", "
+							+ String.format("%.2f", item.total()));
+					bw.newLine();
+				}
+
+				System.out.println(targetFile + " Created!");
+			} catch (IOException e) {
+				System.out.println("Error writng file: " + e.getMessage());
+			}
+		} catch (IOException e) {
+			System.out.println("Error reading file: " + e.getMessage());
+		}
 
 		sc.close();
 	}
