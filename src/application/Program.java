f17da36b13640fe08cbef686408957582ef48d78
Michel Morais <valentimmichel@gmail.com>
2023-02-26 11:04:15 -0300

    my new version

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,52 +14,54 @@ import java.util.Scanner;
 import entities.Product;
 
 public class Program {
-//Exercicio proposto
+
 	public static void main(String[] args) {
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		List<Product> list = new ArrayList<>();
+		List<Product> list = new ArrayList<Product>();
 
-		System.out.print("Enter a file path: ");
-		String strFile = sc.nextLine();
+		System.out.print("Enter file path: ");
+		String strPath = sc.nextLine();
 
-		File file = new File(strFile);
-		String strFolder = file.getParent();
+		File file = new File(strPath);
+		String onlyPath = file.getParent();
 
-		boolean success = new File(strFolder + "\\out").mkdir();//Criação de uma subpasta "out"
-		String targetFile = strFolder + "\\out\\summary.csv";
+		boolean success = new File(onlyPath + "\\out").mkdir();
+		String fullPath = onlyPath + "\\out\\summary.csv";
 
-		try (BufferedReader br = new BufferedReader(new FileReader(strFile))) {
+		try (BufferedReader br = new BufferedReader(new FileReader(strPath))) {
 
-			String itemCsv = br.readLine();
-			while (itemCsv != null) {
-
-				String[] fields = itemCsv.split(",");
+			String read = br.readLine();
+			while (read != null) {
+				String[] fields = read.split(",");
 				String name = fields[0];
 				double price = Double.parseDouble(fields[1]);
 				int quantity = Integer.parseInt(fields[2]);
-
+				
 				list.add(new Product(name, price, quantity));
 
-				itemCsv = br.readLine();
+				read = br.readLine();
 			}
-			try (BufferedWriter bw = new BufferedWriter(new FileWriter(targetFile))) {
+			try (BufferedWriter bw = new BufferedWriter(new FileWriter(fullPath))) {
 
 				for (Product item : list) {
-					bw.write(item.getName() + ", "
-							+ String.format("%.2f", item.total()));
+					bw.write(item.getName() + ", " + String.format("%.2f", item.total()));
 					bw.newLine();
 				}
 
-				System.out.println(targetFile + " Created!");
+				System.out.println(fullPath + " Created!");
+				
 			} catch (IOException e) {
-				System.out.println("Error writng file: " + e.getMessage());
+				System.out.println("File writing error " + e.getMessage());
 			}
 		} catch (IOException e) {
-			System.out.println("Error reading file: " + e.getMessage());
+			System.out.println("Error reading the file " + e.getMessage());
+		} finally {
+			if (sc != null) {
+				sc.close();
+			}
 		}
-
-		sc.close();
 	}
 }
